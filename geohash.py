85573177bf7987c17f51b8c0cd983909c7e32d64
watchboy <watchboy.es@gmail.com>
2010-03-06 11:34:38 -0500

    Pseudocode

--- geohash.py
+++ geohash.py
@@ -1,4 +1,25 @@
-# Input
-inStr = raw_input('Enter your longitude: ')
-while True:
-    long = 
+# Generates random coordinates
+
+# Get present coordinates
+## User input
+### presLong
+### presLat
+## Remove anything beyond the decimal point
+### longInt
+### latInt
+
+# Get destination decimals
+## Collect Data
+### Get date
+### Get DOW opening
+## Hash data (md5)
+## Split hash into two
+## Convert to decimal values
+### longDec
+### latDec
+
+# Combine into final coordinates
+## finalLong
+## finalLat
+
+# Translate coordinates to destination
