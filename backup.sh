0e23a154d006d342c9f15f67468567f5715bd01a
Watchboy <watchboy.es@gmail.com>
2010-11-22 19:26:29 -0500

    Pulls variables from file, backup.conf by default

--- backup.sh
+++ backup.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 # To Do:
 #   Accept arguments for alternate home and target directories
+#   Argument for alternate config file
 #   Switch to move Videos directory
 
-homeDir='/home/watchboy/'
-targetDir='/media/Backup/'
+configFile='backup.conf'
+homeDir="`cat $configFile | grep homeDir | sed 's/homeDir=//'`"
+targetDir="`cat $configFile | grep targetDir | sed 's/targetDir=//'`"
+mysqlUsername="`cat $configFile | grep mysqlUsername | sed 's/mysqlUsername=//'`"
+mysqlPassword="`cat $configFile | grep mysqlPassword | sed 's/mysqlPassword=//'`"
+
 
 apt-cache pkgnames > "$homeDir"Desktop/packages.list
+mysqldump --all-databases -u "$mysqlUsername" -p "$mysqlPassword" | gzip > "$homeDir"Desktop/MySQL-"`date`".sql.gz
 tar -cvzWf "$targetDir"laptop-"`date`".tar.gz /home/watchboy --exclude="$homeDir"Music --exclude="$homeDir"Podcasts --exclude="$homeDir"Videos
 #mv -v "$homeDir"Videos "$targetDir" 
