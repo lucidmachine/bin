ef332b0f5729d21506e4e267f249af6e1293afa3
Watchboy <watchboy.es@gmail.com>
2011-01-04 14:26:21 -0500

    Added permissions check, changed from remove to purge Modularized code Initial commit

--- cleanKernels.sh
+++ cleanKernels.sh
@@ -1,9 +1,19 @@
-#/bin/bash
+#!/bin/bash
+
+# Require root permissions
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    sudo $0 "$@"
+    exit
+fi
+
 ls /boot/ | grep vmlinuz | sed 's@vmlinuz-@linux-image-@g' | grep -v `uname -r` > /tmp/kernelList
 for I in `cat /tmp/kernelList`
 do
-    aptitude remove $I
+    printf "Uninstalling ${I}...\n\n"
+    apt-get -y purge $I
+    printf "\n\n"
 done
 rm -f /tmp/kernelList
-update-grub
 
+printf "Updating grub...\n\n"
+update-grub
--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 
+# Variables
+SOURCEDIR=""
+DESTDIR="/home/watchboy/Downloads/Andy Thomas/Blood And Sunshine"
+
+# Functions
+# #############################################################################
+# Name: Convert
+#
+# Purpose: Convert the given .flac file
+# Receive: .flac filename, destination directory
+# #############################################################################
+function convert {
+    # Metadata
+    ARTIST=`metaflac "$1" --show-tag=ARTIST | sed s/.*=//g`
+    TITLE=`metaflac "$1" --show-tag=TITLE | sed s/.*=//g`
+    ALBUM=`metaflac "$1" --show-tag=ALBUM | sed s/.*=//g`
+    GENRE=`metaflac "$1" --show-tag=GENRE | sed s/.*=//g`
+    TRACKNUMBER=`metaflac "$1" --show-tag=TRACKNUMBER | sed s/.*=//g`
+    DATE=`metaflac "$1" --show-tag=DATE | sed s/.*=//g`
+    
+    # Filename
+    NUM="$TRACKNUMBER"
+    if ((NUM < 10)); then
+        NUM=""0""$TRACKNUMBER""
+    fi
+    OUTF=""$2"/"$ARTIST"/"$ALBUM"/"$NUM" - "$TITLE".mp3"
+
+    # Create Target Directory
+    TDIR=""$2"/"$ARTIST"/"$ALBUM""
+    if [ ! -d "$TDIR" ]; then
+        mkdir -p "$TDIR"
+    fi
+    
+    # Encoding
+    # LAME encoding switches:
+        # -m j      : Joint-stereo mode
+        # - q 0      : Highest quality, lowest speed
+        # --vbr-new : Use newest algorithm for speed boost
+        # -V 0      : Highest VBR quality
+        # -s 44.1   : Sample at 44.1 kHz
+    flac --stdout --decode "$1" | lame -m j -q 0 --vbr-new -V 0 -s 44.1 - "$OUTF"
+    id3 -t "$TITLE" -T "${TRACKNUMBER:-0}" -a "$ARTIST" -A "$ALBUM" -y "$DATE" -g "${GENRE:-12}" "$OUTF"
+}
+
 for a in *.flac
-
 do
-OUTF=`echo "$a" | sed s/\.flac$/.mp3/g`
-
-ARTIST=`metaflac "$a" --show-tag=ARTIST | sed s/.*=//g`
-TITLE=`metaflac "$a" --show-tag=TITLE | sed s/.*=//g`
-ALBUM=`metaflac "$a" --show-tag=ALBUM | sed s/.*=//g`
-GENRE=`metaflac "$a" --show-tag=GENRE | sed s/.*=//g`
-TRACKNUMBER=`metaflac "$a" --show-tag=TRACKNUMBER | sed s/.*=//g`
-DATE=`metaflac "$a" --show-tag=DATE | sed s/.*=//g`
-
-flac -c -d "$a" | lame -m j -q 0 --vbr-new -V 0 -s 44.1 - "$OUTF"
-id3 -t "$TITLE" -T "${TRACKNUMBER:-0}" -a "$ARTIST" -A "$ALBUM" -y "$DATE" -g "${GENRE:-12}" "$OUTF"
-
+    convert "$a" "$DESTDIR"
 done
