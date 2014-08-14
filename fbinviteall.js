da61df7737f968206de4f3706f1e701d699bb194
watchboy <watchboy.es@gmail.com>
2014-08-13 21:29:14 -0400

    Cleaning out old scripts.

--- fbinviteall.js
+++
@@ -1,3 +0,0 @@
-# Paste into url bar while on group/event page to systematically select all friends
-
-javascript:elms=document.getElementById('friends').getElementsByTagName('li');for(var fid in elms){if(typeof elms[fid] === 'object'){fs.click(elms[fid]);}}
