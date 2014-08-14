f08e87515a1e329f4e9990f91f4a4fb3355f905b
watchboy <watchboy.es@gmail.com>
2014-08-13 21:42:36 -0400

    Stuff\!

--- m4a2mp3.sh
+++ m4a2mp3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for i in *.m4a
-do
-OUTF=`echo "$i" | sed s/\.m4a$/.mp3/g`
-ffmpeg -v 5 -y -i "$i" -acodec libmp3lame -ac 2 "$OUTF"
+for i in *.m4a; do
+    OUTF=`echo "$i" | sed s/\.m4a$/.mp3/g`
+    ffmpeg -v 5 -y -i "$i" -acodec libmp3lame -ac 2 "$OUTF"
 done
