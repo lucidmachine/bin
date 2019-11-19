48f337a25b86490901387d6cbeeaaae15d525e52
lucidmachine <lucid.machine@gmail.com>
2019-11-18 19:11:44 -0500

    Line breaks.

--- sortpkgsize.sh
+++ sortpkgsize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Sorts installed .deb packages by size, large to small
 
-dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\n' | sort -k2 -nr | less
+dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\n' \
+    | sort -k2 -nr \
+    | less
