f6b1ed136242a19225c38b68f6c270d2f7d823b1
lucidmachine <lucid.machine@gmail.com>
2019-12-13 11:19:47 -0500

    Scope to a single file

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
+# Converts a given .flac audio file to .mp3
 
-# Variables
-SOURCEDIR=""
-DESTDIR="/home/watchboy/Downloads/Andy Thomas/Blood And Sunshine"
+function require_bin() {
+    local bin=$1
 
-# Functions
-# #############################################################################
-# Name: Convert
-#
-# Purpose: Convert the given .flac file
-# Receive: .flac filename, destination directory
-# #############################################################################
-function convert {
-    # Metadata
-    ARTIST=`metaflac "$1" --show-tag=ARTIST | sed s/.*=//g`
-    TITLE=`metaflac "$1" --show-tag=TITLE | sed s/.*=//g`
-    ALBUM=`metaflac "$1" --show-tag=ALBUM | sed s/.*=//g`
-    GENRE=`metaflac "$1" --show-tag=GENRE | sed s/.*=//g`
-    TRACKNUMBER=`metaflac "$1" --show-tag=TRACKNUMBER | sed s/.*=//g`
-    DATE=`metaflac "$1" --show-tag=DATE | sed s/.*=//g`
-    
-    # Filename
-    NUM="$TRACKNUMBER"
-    if ((NUM < 10)); then
-        NUM=""0""$TRACKNUMBER""
+    if [[ -n $(which ${bin}) ]]; then
+        >&2 echo "${bin} not installed"
+        exit 1
     fi
-    OUTF=""$2"/"$ARTIST"/"$ALBUM"/"$NUM" - "$TITLE".mp3"
-
-    # Create Target Directory
-    TDIR=""$2"/"$ARTIST"/"$ALBUM""
-    if [ ! -d "$TDIR" ]; then
-        mkdir -p "$TDIR"
-    fi
-    
-    # Encoding
-    # LAME encoding switches:
-        # -m j      : Joint-stereo mode
-        # - q 0      : Highest quality, lowest speed
-        # --vbr-new : Use newest algorithm for speed boost
-        # -V 0      : Highest VBR quality
-        # -s 44.1   : Sample at 44.1 kHz
-    flac --stdout --decode "$1" | lame -m j -q 0 --vbr-new -V 0 -s 44.1 - "$OUTF"
-    id3 -t "$TITLE" -T "${TRACKNUMBER:-0}" -a "$ARTIST" -A "$ALBUM" -y "$DATE" -g "${GENRE:-12}" "$OUTF"
 }
 
-for a in *.flac
-do
-    convert "$a" "$DESTDIR"
-done
+require_bin 'metaflac'
+require_bin 'flac'
+require_bin 'lame'
+require_bin 'id3'
+
+flac_filename=$1
+mp3_filename=$(echo ${flac_filename} | sed -e 's/\.flac$/.mp3/')
+
+# Metadata
+artist=`metaflac "${flac_filename}" --show-tag=ARTIST | sed s/.*=//g`
+title=`metaflac "${flac_filename}" --show-tag=TITLE | sed s/.*=//g`
+album=`metaflac "${flac_filename}" --show-tag=ALBUM | sed s/.*=//g`
+genre=`metaflac "${flac_filename}" --show-tag=GENRE | sed s/.*=//g`
+tracknumber=`metaflac "${flac_filename}" --show-tag=TRACKNUMBER | sed s/.*=//g`
+date=`metaflac "${flac_filename}" --show-tag=DATE | sed s/.*=//g`
+
+# Encoding
+# LAME encoding switches:
+    # -m j      : Joint-stereo mode
+    # - q 0     : Highest quality, lowest speed
+    # --vbr-new : Use newest algorithm for speed boost
+    # -V 0      : Highest VBR quality
+    # -s 44.1   : Sample at 44.1 kHz
+flac --stdout --decode "${flac_filename}" \
+    | lame -m j -q 0 --vbr-new -V 0 -s 44.1 - "${mp3_filename}"
+id3 \
+    -t "$title" \
+    -T "${tracknumber:-0}" \
+    -a "$artist" \
+    -A "$album" \
+    -y "$date" \
+    -g "${genre:-12}" \
+    "${mp3_filename}"
