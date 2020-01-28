6a31ff36294f76b7795498a2f4ccc7c4d8f59596
lucidmachine <lucid.machine@gmail.com>
2020-01-27 20:56:08 -0500

    mine.sh: Accept multiple targets

--- mine.sh
+++ mine.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# Makes the current user the owner of the target file or directory
+# Makes the current user the owner of the target files or directories
 
 current_user=$(whoami)
-target=$1
+targets=$@
 
-sudo chown -R ${current_user} ${target}
-sudo chgrp -R ${current_user} ${target}
+for target in "${targets}"; do
+  sudo chown -R ${current_user} ${target}
+  sudo chgrp -R ${current_user} ${target}
+done
